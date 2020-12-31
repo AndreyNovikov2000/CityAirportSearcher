f0d98f12bcf4dc5487c660977433116f7693410a
Novikov Andrey <novikovandrey2000@gmail.com>
2020-12-31 22:03:58 +0600

    Initial Commit

---
+++ CityAirportSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityAirportSearcher
+//
+//  Created by Andrey Novikov on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
